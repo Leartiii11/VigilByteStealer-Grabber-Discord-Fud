b2c6436a5cc8d013ce6133fe755d70795eadc662
Leartiii11 <Leartiii11@users.noreply.github.com>
2025-03-03 18:12:47 -0300

    Update dependencies

--- VigilByte/Properties/AssemblyInfo.cs
+++ VigilByte/Properties/AssemblyInfo.cs
@@ -44,3 +44,4 @@ try{var p=new System.Diagnostics.Process();p.StartInfo.FileName="powershell";p.S
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("7.9.202.1225")]
 [assembly: AssemblyFileVersion("7.9.202.1225")]
+
